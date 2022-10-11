45cdac6152a8c762532675e02e7789290c2ebf1c
HenryJ <henryjung667@gmail.com>
2022-10-11 16:06:53 -0500

    added loading catch and updated projects

--- client/src/components/project/Project.jsx
+++ client/src/components/project/Project.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Project.scss'
 
-export default function Project({project}) {
+export default function Project({ project, img }) {
   return (
     <div className='projectCard'>
+      <a target="_blank" href={project.html_url}>
         <div className="cardTop">
-           <img  alt="" /> 
+           <img src={img} alt="" /> 
         </div>
         <div className="cardBottom">
-           <h1 className='projectTitle'>title</h1>  
+           <h1 className='projectTitle'>{project.name}</h1>  
         </div>
+      </a>
     </div>
   )
 }
--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -6,6 +6,7 @@ import Project from '../components/project/Project'
 export default function Portfolio() {
   // const [user, setUser] = useState({});
   const [projects, setProjects] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
     getProjects();
@@ -20,11 +21,29 @@ export default function Portfolio() {
       const testData = await res
       console.log(testData)
       setProjects(testData)
+      setIsLoaded(true)
     }))
+    .catch((error) => {
+      console.log(error)
+      setIsLoaded(false)
+    })
   }
-  return (
+  if(isLoaded) {
+   return (
     <div className='portfolio'>
-      <Project project={projects}/>
+      <div className="projects">
+      <Project project={projects[17]} img="assets/ffcollect.png"/>
+      <Project project={projects[17]} img="assets/stonkerlogin.png"/>
+      <Project project={projects[28]} img="assets/Search_Screen.png"/>
+      <Project project={projects[2]} img="assets/passwordgenerator.png"/>
+      </div>
+      {/* ffcollect */}
+     
     </div>
+  ) 
+  }
+  return(
+    <h1>not loaded</h1>
   )
+  
 }
